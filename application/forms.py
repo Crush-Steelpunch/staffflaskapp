3f61ba0c8d0e8977be1b30c794122d69cb96b814
Leon L. Robinson <leon.robinson@qa.com>
2022-03-30 16:44:51 +0100

    added validator

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
 
 class StaffForm(FlaskForm):
-    name = StringField("Name")
+    name = StringField("Name", validators=[DataRequired])
     subjectID = SelectField("Subjects", choices = [])
     submit = SubmitField('Add')
 
